fa2b9edc0e59399b19314004c344d65e475dd1e7
Mehdi <mehdi.lemaire.etu@gmail.com>
2025-04-25 12:27:38 +0200

    projet validé

--- src/components/CardList.jsx
+++ src/components/CardList.jsx
@@ -2,32 +2,7 @@ import React from 'react'
 import '../Styles/Card.sass'
 import Card from './Card'
 
-// La fonction transformItem traite un seul élément avec le mapping fourni
-const transformItem = (item, dataMapping) => {
-  const result = {};
-  
-  // Pour chaque clé dans le mapping
-  Object.keys(dataMapping).forEach(targetKey => {
-    // On récupère le chemin source dans le JSON original
-    const sourcePath = dataMapping[targetKey];
-    
-    // Si c'est une fonction, on l'exécute
-    if (typeof sourcePath === 'function') {
-      result[targetKey] = sourcePath(item);
-    } 
-    // Si c'est une chaîne, on accède à la propriété
-    else if (typeof sourcePath === 'string') {
-      // Support pour les chemins imbriqués comme "user.address.city"
-      const value = sourcePath.split('.').reduce((obj, key) => 
-        obj && obj[key] !== undefined ? obj[key] : undefined, item);
-      result[targetKey] = value;
-    }
-  });
-  
-  return result;
-};
-
-function CardList({ data, urlPath = "logement", mapping }) {
+function CardList({ data, urlPath = "logement", mapToCardProps }) {
   if (!data || data.length === 0) {
     return (
       <div className="container-card">
@@ -38,22 +13,11 @@ function CardList({ data, urlPath = "logement", mapping }) {
     )
   }
 
-  // Si un mapping est fourni, on l'utilise, sinon on utilise le mapToCardProps existant
-  const processedData = data.map(item => {
-    if (mapping) {
-      // Transforme l'élément selon le mapping
-      return transformItem(item, mapping);
-    } else {
-      // Conserve le comportement précédent
-      return item;
-    }
-  });
-
   return (
     <div className="container-card">
       <div className="cards-wrapper">
-        {processedData.map((item) => {
-          const { id, title, cover } = item;
+        {data.map((item) => {
+          const { id, title, cover } = mapToCardProps ? mapToCardProps(item) : item
 
           return (
             <Card
